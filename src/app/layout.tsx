6f228be6d25d534821b6cbfd4e87a6cd89518172
jaaacki <jacki.da@gmail.com>
2025-05-12 23:34:07 +0800

    fix blank page

---
+++ src/app/layout.tsx
@@ -0,0 +1,23 @@
+'use client';
+
+import './globals.css';
+import { Inter } from 'next/font/google';
+import AuthProviderWrapper from '@/components/AuthProviderWrapper';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <AuthProviderWrapper>
+          {children}
+        </AuthProviderWrapper>
+      </body>
+    </html>
+  );
+}
